cd9ea2c62cd9fd72e689ca1b4ed163477a247d54
Patrick Barnes <patrick478@gmail.com>
2013-08-14 14:53:28 +1200

    Add libraries (Crashlytics, Picasso and ActionBarSherlock)

--- src/com/SteelAmbition/Wayfarer/MainActivity.java
+++ src/com/SteelAmbition/Wayfarer/MainActivity.java
@@ -2,14 +2,19 @@ package com.SteelAmbition.Wayfarer;
 
 import android.app.Activity;
 import android.os.Bundle;
+import com.actionbarsherlock.app.SherlockActivity;
+import com.crashlytics.android.Crashlytics;
+import com.squareup.picasso.Picasso;
 
-public class MainActivity extends Activity {
+public class MainActivity extends SherlockActivity {
     /**
      * Called when the activity is first created.
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Crashlytics.start(this);
+
         setContentView(R.layout.main);
     }
 }
