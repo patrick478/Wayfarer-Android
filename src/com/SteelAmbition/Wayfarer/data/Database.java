7d1aa532007ce8e2771b1ce1de569d03df718e99
shroomsmaster666 <haxorjeff@hotmail.com>
2013-10-09 22:54:14 +1300

    Put initial survey in database

--- src/com/SteelAmbition/Wayfarer/data/Database.java
+++ src/com/SteelAmbition/Wayfarer/data/Database.java
@@ -12,6 +12,8 @@ public class Database {
 	private List<Goal> recoveryGoals;
 	private List<Goal> regularGoals;
 	
+	private Survey initialSurvey;
+	
 	public Database(){
 		dangers = new ArrayList<Danger>();
 		dangers.add(new Danger("example_name","",0,new Question("example_question",Arrays.asList(new String[]{"answer_a","answer_b"}))));
@@ -23,7 +25,22 @@ public class Database {
 		recoveryGoals.add(new Goal("recovery_goal_name","recovery_goal_description","recovery_goal_completion_question"));
 		regularGoals = new ArrayList<Goal>();
 		regularGoals.add(new Goal("regular_goal_name","regular_goal_description","regular_goal_completion_question"));
-		
+	}
+	
+	public Survey getInitialDatabase(){
+		List<Question> questions = new ArrayList<Question>();
+		questions.add(new Question("Are they on any medication?",
+				Arrays.asList(new String[]{"None", "Antidepressant", "antipsychotic"})));
+		questions.add(new Question("Do they have any prior troubles with suicide?",
+				Arrays.asList(new String[]{"Yes (more than one)", "Yes (only one)", "No"})));
+		questions.add(new Question("Is there a history of mental illness in their family?",
+				Arrays.asList(new String[]{"Yes", "No", "Not sure"})));
+		questions.add(new Question("How close are you to this person?",
+				Arrays.asList(new String[]{"Not close", "Friend", "Close Friend", "Family"})));
+		questions.add(new Question("Have you already talked to them about this?",
+				Arrays.asList(new String[]{"Yes, they are stable for now", "Yes, but I'm not sure they are stable", "No"})));
+		initialSurvey = new Survey(questions);
+		return initialSurvey;
 	}
 	
 	public String getId(){
